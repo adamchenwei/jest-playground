41c7df479b3f48b3f70794e7203873cf94d49081
Adam Chen Wei <awei@pebblepost.com>
2017-11-21 10:09:48 -0500

    feat(mock): add mocks correctly

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
-// import logo from './logo.svg';
-// import './App.css';
+import logo from './logo.svg';
+import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div> abc</div>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started22, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
     );
   }
 }
