222fddb865749ff9d27090d5fa83babc55821e79
Adam Chen Wei <awei@pebblepost.com>
2017-11-21 10:20:34 -0500

    chore(snapshot): snapshot consider 100% coverage, why

--- src/App.js
+++ src/App.js
@@ -3,9 +3,19 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: 'Adam Chen Wei',
+    };
+  }
   render() {
     return (
       <div className="App">
+        {/* Async Call */}
+        {/* State */}
+        {/* Child Component */}
+        {/* list.map */}
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
--- src/App.test.js
+++ src/App.test.js
@@ -2,13 +2,14 @@ import React from 'react';
 import App from './App';
 import renderer from 'react-test-renderer';
 
-it('renders correctly', () => {
-  console.log('----------------------App');
-  console.log(App);
-  const tree = renderer.create(
-    <App />
-  ).toJSON();
-  console.log('-----_TREEEEE')
-  console.log(tree);
-  expect(tree).toMatchSnapshot();
-});
+
+describe('App Component', () => {
+  test('renders as expected', () => {
+
+    const tree = renderer.create(
+      <App />
+    ).toJSON();
+    console.log(tree);
+    expect(tree).toMatchSnapshot();
+  });
+})
