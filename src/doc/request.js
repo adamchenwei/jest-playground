08a861ff15daac7d5ee5aaa7b0e71bd3369abc85
Adam Chen Wei <awei@pebblepost.com>
2017-12-11 17:34:43 -0500

    fix(WOWOWOOWOWOW): goash, its the fact that I did not place the mock inside the root folder of the test where I placed it at... duhhhhh

--- src/doc/request.js
+++ src/doc/request.js
@@ -1,13 +1,15 @@
-// Copyright 2004-present Facebook. All Rights Reserved.
-
+// request.js
 const http = require('http');
+
 export default function request(url) {
-  console.log('----inside REAL request');
   return new Promise(resolve => {
     // This is an example of an http request, for example to fetch
     // user data from an API.
     // This module is being mocked in __mocks__/request.js
-    //return resolve('abcd');
-    resolve({abb: 'hahahah'})
+    http.get({path: url}, response => {
+      let data = '';
+      response.on('data', _data => (data += _data));
+      response.on('end', () => resolve(data));
+    });
   });
 }
--- src/doc/user.js
+++ src/doc/user.js
@@ -1,9 +1,6 @@
-// Copyright 2004-present Facebook. All Rights Reserved.
-
+// user.js
 import request from './request';
 
 export function getUserName(userID) {
-  console.log('inside actual user');
-  console.log(request.toString());
-  return request('/users/' + userID).then(user => user);
+  return request('/users/' + userID).then(user => user.name);
 }
